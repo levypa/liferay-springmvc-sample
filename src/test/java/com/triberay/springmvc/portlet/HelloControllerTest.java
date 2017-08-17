e332d1ad64a31579daed460e5f648bdf978cb8e1
wvernail <wouter.vernaillen@redtree.be>
2017-08-17 11:36:49 +0200

    added very basic test

--- src/test/java/com/triberay/springmvc/portlet/HelloControllerTest.java
+++ src/test/java/com/triberay/springmvc/portlet/HelloControllerTest.java
@@ -1,4 +1,29 @@
 package com.triberay.springmvc.portlet;
 
+import com.triberay.sprinmvc.testconfig.TestContext;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { TestContext.class, HelloController.class })
+@WebAppConfiguration
 public class HelloControllerTest {
+
+    @Autowired
+    private HelloController helloController;
+
+    @Test
+    public void testHelloControllerBean() {
+        assertNotNull(helloController);
+        assertEquals(
+                "class com.triberay.springmvc.portlet.HelloController",
+                this.helloController.getClass().toString());
+    }
 }
---
+++ src/test/java/com/triberay/sprinmvc/testconfig/TestContext.java
@@ -0,0 +1,7 @@
+package com.triberay.sprinmvc.testconfig;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TestContext {
+}
