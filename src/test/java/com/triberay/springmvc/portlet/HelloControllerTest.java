ae16580367aa6c3272046d36e102bc8eddc3e6f0
wvernail <wouter.vernaillen@redtree.be>
2017-08-17 13:52:07 +0200

    added basic test of render method

--- src/test/java/com/triberay/springmvc/portlet/HelloControllerTest.java
+++ src/test/java/com/triberay/springmvc/portlet/HelloControllerTest.java
@@ -1,20 +1,28 @@
 package com.triberay.springmvc.portlet;
 
-import com.triberay.sprinmvc.testconfig.TestContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { TestContext.class, HelloController.class })
+@ContextConfiguration(classes = { HelloController.class })
 @WebAppConfiguration
 public class HelloControllerTest {
+
+    @Mock
+    private RenderRequest mockRequest;
+    @Mock
+    private RenderResponse mockResponse;
 
     @Autowired
     private HelloController helloController;
@@ -25,5 +33,13 @@ public class HelloControllerTest {
         assertEquals(
                 "class com.triberay.springmvc.portlet.HelloController",
                 this.helloController.getClass().toString());
+    }
+
+    @Test
+    public void testView() {
+
+        String result = helloController.view(mockRequest, mockResponse);
+        assertNotNull(result);
+        assertEquals("view", result);
     }
 }
--- src/test/java/com/triberay/sprinmvc/testconfig/TestContext.java
+++
@@ -1,7 +0,0 @@
-package com.triberay.sprinmvc.testconfig;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class TestContext {
-}
